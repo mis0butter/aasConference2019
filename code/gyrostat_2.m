2bf1b2231184bb1a7cb1ff2e8d10e1f21cbfa2e6
Junette <junetter@gmail.com>
2019-06-20 09:53:55 -0700

    Formatted whitespace

--- code/gyrostat_2.m
+++ code/gyrostat_2.m
@@ -2,10 +2,10 @@ function f=gyrostat_2(kp,kd,x,t,omega_c,omegadot_c,q_c,Jnom)
 global torq
 % Kinematic and EoMs of a Rigid Gyrostat
 
-q=[x(1);x(2);x(3);x(4)]; % Gyrosdtat quaternions: [q1,q2,q3,q4]
+q=[x(1);x(2);x(3);x(4)]; % Gyrostat quaternions: [q1,q2,q3,q4]
 omega=[x(5);x(6);x(7)]; % Gyrostat angular velocity components
-wh=[x(8);x(9);x(10)]; % Wheel momenta
-q_cc=[x(11);x(12);x(13);x(14)]; % computed commanded quarternions
+wh=[x(8);x(9);x(10)]; % Wheel momentum
+q_cc=[x(11);x(12);x(13);x(14)]; % computed commanded quaternions
 
 torq=NLC_fun(kp,kd,omega,omega_c,omegadot_c,q,q_cc,Jnom);
 % Integration Functions
@@ -20,6 +20,6 @@ Omega_c=[-skew_omega_c omega_c;-omega_c' 0]; % Eq. (2.97a)
 f(1:4,:)=0.5*Omega*q;  % Eq. (7.1)
 f(5:7,:)=Jnom\(-skew_omega*Jnom*omega+torq);% Eq. (7.16a)
 f(8:10,:)=-skew_omega*wh-torq;% Wheel torque; Eq. (7.16b)
-% Computing the commanded quarternions from the commanded angular velocity
-f(11:14,:)=0.5*Omega_c*q_cc;% commanded quarternion
+% Computing the commanded quaternions from the commanded angular velocity
+f(11:14,:)=0.5*Omega_c*q_cc;% commanded quaternion
 
