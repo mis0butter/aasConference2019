76d1d2cc94109e5b982bc0ecf1b2f63a5647150a
Junette <junetter@gmail.com>
2019-07-09 02:15:12 -0700

    Fixed cross product to find sun projection vector. Transformed sun and position vectors to unit right after variable declaration

--- code/pseudo_code.m
+++ code/pseudo_code.m
@@ -1,5 +1,10 @@
 % MAIN Example 
+% Junette Hsin 
+% Mohammad Ayoubi 
+
 close all; 
+
+%% Inputs 
 
 % Non-changing parameters; should be function inputs 
 inertia = [ 408     0       0; 
@@ -7,28 +12,35 @@ inertia = [ 408     0       0;
             0       0       305]; 
 Pi_G = [1; 0; 0];                           % Pi = unit vector of initial point in the G frame 
 Pf_G = [0; 1; 0];                           % Pf = unit vector of the final point in the G frame 
-S_N = [cosd(45); sind(45); 0];              % S = unit vector of sun vector in the N frame 
+S_N = [cosd(45); cosd(45); cosd(45)];       % S = unit vector of sun vector in the N frame 
+S_N = S_N/norm(S_N);                        % normalizing sun vector 
 G_DCM_N = eye(3);                           % DCM from the N to the G frame 
 N_DCM_G = G_DCM_N';                         % G to N frame 
 S_G = G_DCM_N*S_N;                          % Sun vector in G frame 
 ep = pi/12;                                 % payload half-cone angle. pi/12 rad = 15 deg  
+aMax = 1;                                  % Maximum acceleration, rad/s^2
+wMax = 1;                                  % Maximum angular velocity, rad/s
+
+%% Calculate slew angles 
 
 % Calculate normal vector of slew plane 
 e_G = cross(Pi_G, Pf_G) / norm(cross(Pi_G, Pf_G));  % eigenaxis of PiPf plane, in G frame 
-Pperp_G = cross(e_G, Pi_G/norm(Pi_G));              % perpendicular vector to e and Pi, slew plane, G frame 
-P_DCM_G = [(Pi_G/norm(Pi_G))'; Pperp_G'; e_G'];     % DCM from G frame to P (slew plane) frame 
+Pperp_G = cross(Pi_G, e_G);              % perpendicular vector to e and Pi, slew plane, G frame 
+P_DCM_G = [Pi_G'; Pperp_G'; e_G'];     % DCM from G frame to P (slew plane) frame 
 G_DCM_P = P_DCM_G';                         % P to G frame 
 
 % Check angular separation between sun vector S and slew plane 
-alpha = pi/2 - acos(dot(S_G, e_G)); 
+alpha = pi/2 - acos(dot(S_G, e_G));         % coming out to 0 - check
+% alpha = pi/4; 
 
 %%%%%%
 % IF angular separation is less than payload half-cone angle --> find phi2
 % and phi3. Otherwise, slew is just phi1. 
 %%%%%%
 
-unit_S = S_G/norm(S_G); 
-S_PiPf_G = cross(cross(unit_S, e_G), e_G);  % sun projection vector G frame 
+% unit_S = S_G/norm(S_G); 
+S_PiPf_G = -cross(cross(S_G, e_G), e_G);    % sun projection vector G frame
+S_PiPf_G = S_PiPf_G/norm(S_PiPf_G);         % sun projection --> unit vector 
 
 % First slew around eigenaxis
 phi1 = acos(dot(Pi_G, S_PiPf_G)) - ep;      % dot product of unit vectors 
@@ -51,15 +63,15 @@ if alpha == 0
     phi2 = pi; 
 else 
     % Not sure how to get the one below: 
-%     theta = acos(dot(Pi_G, S_N)); 
-%     phi2 = 2*asin(sin(ep)/sin(theta)); 
+    theta = acos(dot(Pi_G, S_N)); 
+    phi2_M = 2*asin(sin(ep/2)/sin(theta/2)); 
 
     % Junette's derived: 
     theta = acos(dot(S_G, P1_G));           % angle btwn sun and P1 vectors 
-    P3_G = S_G*P1_G*cos(theta);             % P3 vector in G frame (S and P1 already unit vectors)
+    P3_G = S_G*norm(P1_G)*cos(theta);             % P3 vector in G frame (S and P1 already unit vectors)
     P3P1_G = P1_G - P3_G;                   % vector from P3 to P1 
     P3P2_G = P2_G - P3_G;                   % vector from P3 to P2 
-    phi2 = acos(dot(P3P1_G, P3P2_G));       % slew around sun vector 
+    phi2 = acos(dot(P3P1_G/norm(P3P1_G), P3P2_G/norm(P3P2_G)));       % slew around sun vector 
 end 
 
 % Slew around eigenvector via phi3 
@@ -69,29 +81,39 @@ if phi3 > pi/2
     phi3_rem = phi3 - pi/2; 
 end 
 
+%% Determine slew times 
 
+t0 = 0; 
+tPhi1 = t0 + wMax/aMax; 
 
-%%
+% Mohammad's equation 
+tPhi2 = tPhi1 + (1/wMax) * ... 
+    ( phi1 - 0.5*aMax(tPhi1)^2 - ... 
+    (wMax*wMax)/aMax + (wMax)^2/(2*aMax) ); 
+
+tPhi3 = tPhi2 + tPhi1; 
+
+%% Solve for attitue determination 
         
-t = 100; 
+tEnd = 100; 
 
-phi_1_accel = [ 2*(phi1/2)/t^2;  0;  0];  
+phi_1_accel = [ 2*(phi1/2)/tEnd^2;  0;  0];  
 torque = inertia*phi_1_accel; 
 
 w_in = [    0.5;    0;      0]; 
 q_in = [    0;      0;      0;      1]; 
 
-[t1, y1] = ode45(@(t,Z) gyrostat_cont(inertia, torque, Z), [0, 100], [w_in; q_in]);
+[t1, y1] = ode45(@(t,Z) gyrostat_cont(inertia, torque, Z), [0, tEnd], [w_in; q_in]);
 
 w_in = y1(end, 1:3)'; 
 q_in = y1(end, 4:7)'; 
 
-[t2, y2] = ode45(@(t,Z) gyrostat_cont(inertia, -torque, Z), [0, 100], [w_in; q_in]); 
+[t2, y2] = ode45(@(t,Z) gyrostat_cont(inertia, -torque, Z), [0, tEnd], [w_in; q_in]); 
 
 y = [y1; y2]; 
 t = [t1; t2]; 
 
-%%
+%% Plot 
 
 w = y(:, 1:3); 
 ylimits = get_ylimits(w); 
