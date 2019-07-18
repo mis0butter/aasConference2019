2c0c5db87123549d21d3c8535cec98af2f4dc1c9
Junette Hsin <junetter@gmail.com>
2019-07-18 08:44:12 -0700

    added total stuff, calc. phi2 times correctly

--- code/main.m
+++ code/main.m
@@ -78,13 +78,14 @@ else
     SP1_G = P1_G - S_G; 
     SP2_G = P2_G - S_G; 
     phi2_S = acos(dot(SP1_G/norm(SP1_G), SP2_G/norm(SP2_G))); 
+    phi2 = phi2_S; 
 end 
 
 %% optional plotting routine to check things 
 
 close all; 
 
-plot_option = 1; 
+plot_option = 0; 
 if plot_option == 1
     figure()
         plot3([0 P1_G(1)], [0 P1_G(2)], [0 P1_G(3)], 'b'); 
@@ -156,7 +157,7 @@ t3 = t2 - (wf - wMax)/aMax;
 % t0 --> t1 
 tEnd = t1 - t0; 
 w_in = [    0;    0;      0]; 
-q_in = [    0;      0;      0;      1]; 
+q_in = [    0;      0;      0;      1];             % wrt G frame 
 a = [ aMax;  0;  0];  
 torque = inertia*a; 
 
@@ -196,6 +197,8 @@ t_phi1 = [t1_phi1; t1_phi1(end)+t2_phi1(2:end); t1_phi1(end)+t2_phi1(end)+t3_phi
 % ylimits = get_ylimits(w); 
 
 % Plot 
+
+if plot_option == 1
 figure()
     plot(t_phi1, w_phi1) 
 %     ylim(ylimits)
@@ -218,29 +221,55 @@ figure()
     xlabel('time (s)') 
     ylabel('degrees') 
     title('Euler Angles Phi 1') 
+end 
     
 %% Determine Phi 2 slew times 
 
-% idk 
-tEnd = 2; 
+w0 = 0; 
+t0 = 0; 
+wf = 0; 
+
+t1 = t0 + (wMax-w0)/aMax; 
+
+% Mohammad's equation 
+t2 = t1 - (1/wMax) * ... 
+    ( phi2 - w0*(t1 - t0) - 0.5*aMax*(t1 - t0)^2 ... 
+    - wMax*(wMax - wf)/aMax + (wMax - wf)^2/(2*aMax) );  
+
+% % Phi1 times 
+% t1_Phi1 = wMax/aMax; 
+% t2_Phi1 = Phi1/wMax; 
+% t3_Phi1 = t1_Phi1 + t2_Phi1; 
+
+t3 = t2 - (wf - wMax)/aMax; 
     
 %% Solve for attitude determination - second slew 
 
-a = aMax*S_G; 
-torque = inertia*a; 
+tEnd = t1 - t0; 
 w_in = w_phi1(end, :)'; 
 q_in = q_phi1(end, :)'; 
+a = aMax*S_G; 
+torque = inertia*a; 
 
 [t1_phi2, y1_phi2] = ode45(@(t,Z) gyrostat_cont(inertia, torque, Z), [0, tEnd], [w_in; q_in]);
 
+tEnd = t2 - t1; 
 w_in = y1_phi2(end, 1:3)'; 
 q_in = y1_phi2(end, 4:7)'; 
-a = aMax*-S_G; 
+a = [0; 0; 0]; 
 torque = inertia*a; 
 
 [t2_phi2, y2_phi2] = ode45(@(t,Z) gyrostat_cont(inertia, torque, Z), [0, tEnd], [w_in; q_in]);
 
-y_phi2 = [y1_phi2; y2_phi2(2:end, :)]; 
+tEnd = t3 - t1; 
+w_in = y2_phi2(end, 1:3)'; 
+q_in = y2_phi2(end ,4:7)'; 
+a = -aMax*S_G; 
+torque = inertia*a; 
+
+[t3_phi2, y3_phi2] = ode45(@(t,Z) gyrostat_cont(inertia, torque, Z), [0, tEnd], [w_in; q_in]);
+
+y_phi2 = [y1_phi2; y2_phi2(2:end, :); y3_phi2(2:end, :)]; 
 w_phi2 = y_phi2(:, 1:3); 
 q_phi2 = y_phi2(:, 4:7); 
 
@@ -250,13 +279,14 @@ for i = 1:max(size(q_phi2))
     ypr_phi2(i, :) = SpinCalc('QtoEA321', q_phi2(i, :), eps, 0); 
 end 
 
-t_phi2 = [t1_phi2; t1_phi2(end) + t2_phi2(2:end)]; 
+t_phi2 = [t1_phi2; t1_phi2(end) + t2_phi2(2:end); t1_phi2(end) + t2_phi2(end) + t3_phi2(2:end)]; 
 
 %% Plot phi2
 
 w = y_phi2(:, 1:3); 
 q = y_phi2(:, 4:7); 
 
+if plot_option == 1
 figure()
     plot(t_phi2, w)
 %     ylim(ylimits)
@@ -279,7 +309,7 @@ figure()
     xlabel('time (s)') 
     ylabel('degrees') 
     title('Euler Angles Phi 2') 
-    
+end 
 
 
 %% Determine Phi 3 slew times
@@ -347,6 +377,7 @@ t_phi3 = [t1_phi3; t1_phi3(end)+t2_phi3(2:end); t1_phi3(end)+t2_phi3(end)+t3_phi
 % ylimits = get_ylimits(w); 
 
 % Plot 
+if plot_option == 1
 figure()
     plot(t_phi3, w_phi3) 
 %     ylim(ylimits)
@@ -369,6 +400,59 @@ figure()
     xlabel('time (s)') 
     ylabel('degrees') 
     title('Euler Angles Phi 3') 
+end 
+
+%% total slew stuff 
+
+t_total = [ t_phi1; ... 
+            t_phi1(end) + t_phi2(2:end); ... 
+            t_phi1(end) + t_phi2(end) + t_phi3(2:end)]; 
+        
+w_total = [ w_phi1; ... 
+            w_phi2(2:end, :); ... 
+            w_phi3(2:end, :)]; 
+        
+q_total = [ q_phi1; ... 
+            q_phi2(2:end, :); ... 
+            q_phi3(2:end, :)]; 
+        
+ypr_total = zeros(length(q_total), 3); 
+for i = 1:max(size(q_total))
+    ypr_total(i, :) = SpinCalc('QtoEA321', q_total(i, :), eps, 0); 
+end 
+        
+%% plot total stuff 
+        
+plot_total = 1; 
+if plot_total == 1
+
+figure()
+plot(t_total, w_total) 
+%     ylim(ylimits)
+    grid on
+    legend('w1', 'w2', 'w3'); 
+    ylabel('w (rad/s)') 
+    xlabel('time (s)') 
+    title('Angular Velocity Total') 
+
+figure()
+    plot(t_total, q_total)
+    grid on 
+    legend('q1', 'q2', 'q3', 'q4'); 
+%     ylim(ylimits)
+    ylabel('quats') 
+    xlabel('time (s)') 
+    title('Quaternion Total') 
+    
+figure()
+    plot(t_total, ypr_total)
+    grid on 
+    legend('Yaw', 'Pitch', 'Roll'); 
+    xlabel('time (s)') 
+    ylabel('degrees') 
+    title('Euler Angles Total') 
+    
+end 
 
 %% 
 % 
