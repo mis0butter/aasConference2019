eacc8bd0a52aec45a5056d29f528fdc7f5e7899c
Junette <junetter@gmail.com>
2019-07-21 17:24:17 -0700

    put all plots into plot_everything

---
+++ code/plot_everything.m
@@ -0,0 +1,35 @@
+function plot_everything(t, q, w, ypr, phi_num)
+%% Plot q, w, ypr 
+
+phi_num = num2str(phi_num); 
+
+ylimits_q = get_ylimits(q); 
+ylimits_w = get_ylimits(w); 
+ylimits_ypr = get_ylimits(ypr); 
+
+plot_option = 1; 
+if plot_option == 1
+figure()
+    plot(t, w)
+    ylim(ylimits_w)
+    legend('w1', 'w2', 'w3'); 
+    ylabel('w (rad/s)') 
+    xlabel('time (s)') 
+    title(strcat('Angular Velocity: Phi ', phi_num))
+
+figure()
+    plot(t, q)
+    legend('q1', 'q2', 'q3', 'q4'); 
+    ylim(ylimits_q)
+    ylabel('quats') 
+    xlabel('time (s)') 
+    title(strcat('Quaternion: Phi ', phi_num)) 
+    
+figure()
+    plot(t, ypr)
+    legend('Yaw', 'Pitch', 'Roll'); 
+    ylim(ylimits_ypr)
+    xlabel('time (s)') 
+    ylabel('degrees') 
+    title(strcat('Euler Angles: Phi ', phi_num)) 
+end 
