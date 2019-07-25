c343f081fd6ee2af2d47de270dfd7406985146da
Junette <junetter@gmail.com>
2019-07-25 03:41:50 -0700

    Mohammad's scratch derived slew time eqnas worked!!!!!

--- code/find_slew_times.m
+++ code/find_slew_times.m
@@ -2,11 +2,21 @@ function [t1, t2, t3] = find_slew_times(t0, w0, wf, wMax, aMax, phi, phi_tt)
 
 if phi > phi_tt
 
-    t1 = t0 + (wMax-w0)/aMax;  
-    % Coast t2 --> angular velocity: trapezoid profile 
-    t2 = t1 - (1/wMax) * ... 
-        ( phi - w0*(t1 - t0) - 0.5*aMax*(t1 - t0)^2 ... 
-        - wMax*(wMax - wf)/aMax + (wMax - wf)^2/(2*aMax) );  
+%     t1 = t0 + (wMax-w0)/aMax;  
+%     % Coast t2 --> angular velocity: trapezoid profile 
+%     t2 = t1 - (1/wMax) * ... 
+%         ( phi - w0*(t1 - t0) - 0.5*aMax*(t1 - t0)^2 ... 
+%         - wMax*(wMax - wf)/aMax + (wMax - wf)^2/(2*aMax) );  
+%     t3 = t2 - (wf - wMax)/aMax; 
+    
+    %% Determine Phi slew times - Mohammad's scratch derived equations 
+
+    t1 = t0 + (wMax-w0)/aMax; 
+
+    t2 = t1 + (1/wMax) * ... 
+        ( phi + wMax*(wf - wMax)/aMax + 0.5*aMax*((wf - wMax)/aMax)^2 ... 
+        - aMax/2*(t1 - t0)^2 - w0*(t1 - t0) ); 
+
     t3 = t2 - (wf - wMax)/aMax; 
 
 else 
