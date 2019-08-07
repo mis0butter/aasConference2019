8b4bda01f290b0626f0536b5c4c724108b57aabd
Junette <junetter@gmail.com>
2019-08-06 22:01:34 -0700

    more comments in post_processing

--- code/post_processing.m
+++ code/post_processing.m
@@ -79,15 +79,22 @@ end
 %%
 
 figure()
-    plot3(P_G(:, 1), P_G(:, 2), P_G(:,3)); 
-%     plot3(P_Gnom(:, 1), P_Gnom(:, 2), P_Gnom(:, 3), '-.')
-    grid on; hold on
-%     plot3(P_G1_phi2(:, 1), P_G1_phi2(:, 2), P_G1_phi2(:, 3), 'r'); 
-%     plot3(P_G2_phi2(:, 1), P_G2_phi2(:, 2), P_G2_phi2(:, 3), 'g');
-%     plot3(P_G3_phi2(:, 1), P_G3_phi2(:, 2), P_G3_phi2(:, 3), 'r');
-%     plot3(P_Gnom(:, 1), P_Gnom(:, 2), P_Gnom(:,3), '-.'); 
-%     plot3(phi1_P(:, 1), phi1_P(:, 2), phi1_P(:, 3), '-.'); 
 
+    % Actual Slew Profile 
+    plot3(P_G(:, 1), P_G(:, 2), P_G(:,3)); 
+    
+    grid on; hold on
+    
+    % Phi2 Slew Profile 
+    plot3(P_G1_phi2(:, 1), P_G1_phi2(:, 2), P_G1_phi2(:, 3), 'r'); 
+    plot3(P_G2_phi2(:, 1), P_G2_phi2(:, 2), P_G2_phi2(:, 3), 'g');
+    plot3(P_G3_phi2(:, 1), P_G3_phi2(:, 2), P_G3_phi2(:, 3), 'r');
+    
+    % Nominal (no sun intrusion) profile 
+%     plot3(P_Gnom(:, 1), P_Gnom(:, 2), P_Gnom(:,3), '-.'); 
+
+    % Phi1 and 3 profiles - taken from nominal profile 
+%     plot3(phi1_P(:, 1), phi1_P(:, 2), phi1_P(:, 3), '-.'); 
 %     plot3(P_phi3_G0(:, 1), P_phi3_G0(:, 2), P_phi3_G0(:, 3), '-.'); 
 %     plot3(P_phi1_G0(:, 1), P_phi1_G0(:, 2), P_phi1_G0(:, 3), '-.'); 
     
@@ -103,20 +110,21 @@ figure()
 %     plot3([0 S2_G0(1)], [0 S2_G0(2)], [0 S2_G0(3)])
 %     plot3([0 S3_G0(1)], [0 S3_G0(2)], [0 S3_G0(3)])
 
-%     plot3(P_G0(:, 1), P_G0(:, 2), P_G0(:, 3), '-.') 
 %     plot3(P_S(:, 1), P_S(:, 2), P_S(:, 3))
     
     plot(theta,sin(theta)./theta,'LineWidth',3) 
     
+    % Sun vector 
     plot3([0 S_G0(1)], [0 S_G0(2)], [0 S_G0(3)], 'r'); 
+    % Sun projection 
     plot3([0 S_PiPf_G0(1)], [0 S_PiPf_G0(2)], [0 S_PiPf_G0(3)], 'r'); 
     
+    % Phi2 - P3 (P1,P2 proj onto Sun vector) lines 
     plot3([0 P3_G0(1)], [0 P3_G0(2)], [0 P3_G0(3)], 'b'); 
     plot3([P3_G0(1) P1_G0(1)], [P3_G0(2) P1_G0(2)], [P3_G0(3) P1_G0(3)], 'r'); 
     plot3([P3_G0(1) P2_G0(1)], [P3_G0(2) P2_G0(2)], [P3_G0(3) P2_G0(3)], 'r'); 
     
-%     text(P3_G0(1), P3_G0(2), P3_G0(3), ... 
-%         sprintf('    phi2 = %0.2f deg', phi2_P3*180/pi))
+    % Initial, final, P2, P2 vectors 
     text(Pi_G0(1), Pi_G0(2), Pi_G0(3), sprintf(' Pi')) 
     text(Pf_G0(1), Pf_G0(2), Pf_G0(3), sprintf(' Pf')) 
     text(e_G0(1)*0.5, e_G0(2)*0.5, e_G0(3)*0.5, sprintf(' e')) 
